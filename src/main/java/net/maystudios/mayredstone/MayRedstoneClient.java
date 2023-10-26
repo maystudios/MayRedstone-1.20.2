4a9d1bdf04cede4e312de9b8a143f5fa92b7f4ed
MayStudios <svenmaibaum21@gmail.com>
2023-10-27 01:27:26 +0200

    first commit

---
+++ src/main/java/net/maystudios/mayredstone/MayRedstoneClient.java
@@ -0,0 +1,10 @@
+package net.maystudios.mayredstone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MayRedstoneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
